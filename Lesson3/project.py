f09d3d9f7bfb791398b930e44850b4835d8494f3
Trish Whetzel <plwhetzel@gmail.com>
2015-07-26 16:48:31 -0700

    add functionality to delete menu item

--- Lesson3/project.py
+++ Lesson3/project.py
@@ -56,9 +56,15 @@ def editMenuItem(restaurant_id, menu_id):
 
 
 # Task 3: Create a route for deleteMenuItem function here
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete', methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return "page to delete a menu item. Task 3 complete!"
+	itemToDelete = session.query(MenuItem).filter_by(id=menu_id).one()
+	if request.method == 'POST':
+		session.delete(itemToDelete)
+		session.commit()
+		return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+	else:
+		return render_template('deletemenuitem.html', restaurant_id=restaurant_id, item=itemToDelete)
 
 
 if __name__ == '__main__':
