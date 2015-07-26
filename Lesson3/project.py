f1f76b664c085c5d95bb7af7fa5af533e2f184d7
Trish Whetzel <plwhetzel@gmail.com>
2015-07-26 15:51:22 -0700

    add edit menu functionality

--- Lesson3/project.py
+++ Lesson3/project.py
@@ -28,27 +28,35 @@ def restaurantMenu(restaurant_id):
 	# return output
 
 # Task 1: Create route for newMenuItem function here
-@app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET', 'POST'])
-
+@app.route('/restaurants/<int:restaurant_id>/new', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
 	if request.method == 'POST':
 		newItem = MenuItem(name = request.form['name'], restaurant_id = restaurant_id)
 		session.add(newItem)
-		session.committ()
+		session.commit()
 		return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
 	else:
 		return render_template('newmenuitem.html', restaurant_id = restaurant_id)
     
 
 # Task 2: Create route for editMenuItem function here
-@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
-
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit',
+           methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template(
+            'editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, i=editedItem)
+
 
 # Task 3: Create a route for deleteMenuItem function here
 @app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
-
 def deleteMenuItem(restaurant_id, menu_id):
     return "page to delete a menu item. Task 3 complete!"
 
